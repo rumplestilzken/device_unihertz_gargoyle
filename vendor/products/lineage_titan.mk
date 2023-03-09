558f2fd15e5334b0f6c1b18bdd17c483e07567d4
rumplestilzken <rumplestilzken@gmail.com>
2023-03-09 03:49:09 +0000

    Initial Commit

---
+++ vendor/products/lineage_titan.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/lineage/gsi/lineage_gsi_arm64_vN.mk)
+
+PRODUCT_RELEASE_NAME := Titan
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := titan
+PRODUCT_BRAND := Unihertz
+PRODUCT_MANUFACTURER := A-gold
+PRODUCT_NAME := Unihertz Titan
+PRODUCT_MODEL := Titan
