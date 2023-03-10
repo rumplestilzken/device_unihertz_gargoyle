58467f0864a9eb948ec8c80af3c26f1b7d1ca377
rumplestilzken <rumplestilzken@gmail.com>
2023-03-10 19:47:17 +0000

    BoardCommon.mk changes

--- vendor/unihertz/products/lineage_titan.mk
+++ vendor/unihertz/products/lineage_titan.mk
@@ -3,8 +3,8 @@ $(call inherit-product, device/lineage/gsi/lineage_gsi_arm64_vN.mk)
 PRODUCT_RELEASE_NAME := Titan
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := titan
+PRODUCT_DEVICE := Titan
 PRODUCT_BRAND := Unihertz
 PRODUCT_MANUFACTURER := A-gold
-PRODUCT_NAME := Unihertz Titan
+PRODUCT_NAME := Titan
 PRODUCT_MODEL := Titan
