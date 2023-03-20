e981bc4422d37371ff1c3cc1760be0b0a238b6aa
rumplestilzken <rumplestilzken@gmail.com>
2023-03-19 18:00:59 -0900

    Removed device tree

--- device/unihertz/AndroidProducts.mk
+++
@@ -1,7 +0,0 @@
-PRODUCT_MAKEFILES += \
-	$(LOCAL_DIR)/lineage_gargoyle_bvN.mk \
-	$(LOCAL_DIR)/lineage_gargoyle_bgN.mk 
-
-COMMON_LUNCH_CHOICES := \
-	lineage_gargoyle_bvN-userdebug \
-	lineage_gargoyle_bgN-userdebug
