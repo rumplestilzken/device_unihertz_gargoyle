4491ff32d02addad63dc478f935c626ab14ed3e2
rumplestilzken <rumplestilzken@gmail.com>
2023-03-19 18:10:37 -0900

    Restructured device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES += \
+	$(LOCAL_DIR)/lineage_gargoyle_bvN.mk \
+	$(LOCAL_DIR)/lineage_gargoyle_bgN.mk 
+
+COMMON_LUNCH_CHOICES := \
+	lineage_gargoyle_bvN-userdebug \
+	lineage_gargoyle_bgN-userdebug
---
+++ lineage_gargoyle_bvN.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/phh/treble/lineage_arm64_bvN.mk)
+$(call inherit-product, device/unihertz/lineage_gargoyle.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gargoyle
+PRODUCT_BRAND := Unihertz
+PRODUCT_SYSTEM_BRAND := Unihertz
+PRODUCT_MANUFACTURER := A-gold
+PRODUCT_NAME := Titan
+PRODUCT_MODEL := Titan
