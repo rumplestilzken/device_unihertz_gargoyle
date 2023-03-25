12727c7312a13b8520753ddce19b98cada6ff5d0
rumplestilzken <rumplestilzken@gmail.com>
2023-03-25 11:37:21 -0900

    Intial work for the Titan Slim and Titan Pocket

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,15 @@
 PRODUCT_MAKEFILES += \
 	$(LOCAL_DIR)/lineage_gargoyle_bvN.mk \
-	$(LOCAL_DIR)/lineage_gargoyle_bgN.mk 
+	$(LOCAL_DIR)/lineage_gargoyle_bgN.mk \
+	$(LOCAL_DIR)/lineage_gargoyle_slim_bvN.mk \
+	$(LOCAL_DIR)/lineage_gargoyle_slim_bgN.mk \
+	$(LOCAL_DIR)/lineage_gargoyle_pocket_bvN.mk \
+	$(LOCAL_DIR)/lineage_gargoyle_pocket_bgN.mk 
 
 COMMON_LUNCH_CHOICES := \
 	lineage_gargoyle_bvN-userdebug \
-	lineage_gargoyle_bgN-userdebug
+	lineage_gargoyle_bgN-userdebug \
+	lineage_gargoyle_slim_bvN-userdebug \
+	lineage_gargoyle_slim_bgN-userdebug \
+	lineage_gargoyle_pocket_bgN-userdebug \
+	lineage_gargoyle_pocket_bgN-userdebug 
---
+++ lineage_gargoyle_slim.mk
@@ -0,0 +1,28 @@
+#Treble
+#PRODUCT_FULL_TREBLE :=  true
+#PRODUCT_FULL_TREBLE_OVERRIDE := true
+
+#PRODUCT_RELEASE_NAME := Titan
+
+# Device identifier. This must come after all inclusions
+#PRODUCT_DEVICE = gargoyle
+#PRODUCT_BRAND = Unihertz
+#PRODUCT_SYSTEM_BRAND = Unihertz
+#PRODUCT_MANUFACTURER = A-gold
+#PRODUCT_NAME = Titan
+#PRODUCT_MODEL = Titan
+
+RESOURCE_PATH := device/unihertz/
+
+#Copy keyboard files into place.
+#PRODUCT_COPY_FILES += \
+#    $(RESOURCE_PATH)/keyboard/system_usr_idc/aw9523-key.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/aw9523-key.idc \
+#	$(RESOURCE_PATH)/keyboard/system_usr_idc/mtk-kpd.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/mtk-kpd.idc \
+#	$(RESOURCE_PATH)/keyboard/system_usr_idc/mtk-pad.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/mtk-pad.idc \
+#	$(RESOURCE_PATH)/keyboard/system_usr_keychars/aw9523-key.kcm:$(TARGET_COPY_OUT_SYSTEM)/usr/keychars/aw9523-key.kcm \
+#	$(RESOURCE_PATH)/keyboard/system_usr_keylayout/Generic.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Generic.kl \
+#	$(RESOURCE_PATH)/keyboard/system_usr_keylayout/mtk-kpd.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mtk-kpd.kl
+
+#Install Kika as system app
+#PRODUCT_PACKAGES += \
+#	Kika-Keyboard_com.iqqijni.bbkeyboard \
