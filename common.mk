6e43133971a2a8e0990358e8e8fd1f44555a868d
rumplestilzken <rumplestilzken@gmail.com>
2023-04-15 03:36:48 -0900

    Updated common.mk service reference
    Change-Id: I239776077aab52bc7c46ab6423cada26f59f338f

--- common.mk
+++ common.mk
@@ -2,4 +2,4 @@ RESOURCE_PATH := device/unihertz/
 
 #Init Scripts
 PRODUCT_COPY_FILES += \
-	$(RESOURCE_PATH)/rumplestilzken_firstboot.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/rumplestilzken_firstboot.sh \
+	$(RESOURCE_PATH)/rumplestilzken_firstboot.rc:$(TARGET_COPY_OUT_SYSTEM)/bin/rumplestilzken_firstboot.rc \
