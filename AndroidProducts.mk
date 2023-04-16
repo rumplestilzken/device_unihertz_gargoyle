b6fa049b7d04c9271a0c8bb3f32323309c580656
rumplestilzken <rumplestilzken@gmail.com>
2023-04-15 18:08:03 -0900

    Added tank to AndroidProducts.mk
    Change-Id: I56938707903adc88b47cfaba0560ebb049ca5e2e

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,7 +4,9 @@ PRODUCT_MAKEFILES += \
 	$(LOCAL_DIR)/lineage_gargoyle_slim_bvN.mk \
 	$(LOCAL_DIR)/lineage_gargoyle_slim_bgN.mk \
 	$(LOCAL_DIR)/lineage_gargoyle_pocket_bvN.mk \
-	$(LOCAL_DIR)/lineage_gargoyle_pocket_bgN.mk 
+	$(LOCAL_DIR)/lineage_gargoyle_pocket_bgN.mk \
+	$(LOCAL_DIR)/lineage_gargoyle_tank_bvN.mk \
+	$(LOCAL_DIR)/lineage_gargoyle_tank_bgN.mk \
 
 COMMON_LUNCH_CHOICES := \
 	lineage_gargoyle_bvN-userdebug \
@@ -12,4 +14,6 @@ COMMON_LUNCH_CHOICES := \
 	lineage_gargoyle_slim_bvN-userdebug \
 	lineage_gargoyle_slim_bgN-userdebug \
 	lineage_gargoyle_pocket_bvN-userdebug \
-	lineage_gargoyle_pocket_bgN-userdebug 
+	lineage_gargoyle_pocket_bgN-userdebug \
+	lineage_gargoyle_tank_bvN-userdebug \
+	lineage_gargoyle_tank_bgN-userdebug \
