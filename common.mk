61f47bbf6a93ec1be64f7956f28606b7038f6235
rumplestilzken <rumplestilzken@gmail.com>
2023-04-18 05:56:53 -0900

    Fix init path for rumplestilzken_firstboot.rc
    Change-Id: I18e9df84c1b8587dd659de00039a33993abb9619

--- common.mk
+++ common.mk
@@ -2,7 +2,7 @@ RESOURCE_PATH := device/unihertz/
 
 #Init Scripts
 PRODUCT_COPY_FILES += \
-	$(RESOURCE_PATH)/rumplestilzken_firstboot.rc:$(TARGET_COPY_OUT_SYSTEM)/bin/rumplestilzken_firstboot.rc \
+	$(RESOURCE_PATH)/rumplestilzken_firstboot.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/rumplestilzken_firstboot.rc \
 
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
