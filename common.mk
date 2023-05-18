ca615256a16096d7ba3583fd8aa74ca4af22fc0e
rumplestilzken <rumplestilzken@gmail.com>
2023-05-18 18:28:15 -0400

    Added gargoyleSettings.apk and include in system image
    Change-Id: I19b7e62eaab560451766922f4ea75660f78722ce

--- common.mk
+++ common.mk
@@ -7,6 +7,8 @@ PRODUCT_COPY_FILES += \
 	$(RESOURCE_PATH)/init/rumplestilzken_firstboot.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/rumplestilzken_firstboot.rc \
 	$(RESOURCE_PATH)/init/rumplestilzken_uinput-titan.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/rumplestilzken_uinput-titan.rc \
 
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
 PRODUCT_PACKAGES += \
     uinput-titan \
     gargoyleSettings
