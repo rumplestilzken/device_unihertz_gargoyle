c579341336bc75d1a322a3e32d13b0538bc35e0d
rumplestilzken <rumplestilzken@gmail.com>
2023-06-28 09:36:16 -0400

    Fixed Kika  dependencies.
    Change-Id: I03fda008b53b7f476881dc3fc782ae076e790b3a

--- common.mk
+++ common.mk
@@ -9,31 +9,6 @@ PRODUCT_COPY_FILES += \
 	$(RESOURCE_PATH)/init/rumplestilzken_firstboot.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/rumplestilzken_firstboot.rc \
 	$(RESOURCE_PATH)/init/rumplestilzken_uinput-titan.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/rumplestilzken_uinput-titan.rc \
 
-#Kika-Input
-KIKA_PATH += $(RESOURCE_PATH)/prebuilt/kika-input/
-#lib
-PRODUCT_PACKAGES += \
-    libChangjieD1 \
-    libChangjieH1 \
-    libChsPinyin \
-    libChtZhuyin \
-    libDutch \
-    libEnglish_GB \
-    libEnglish \
-    libFrench_FR \
-    libGerman \
-    libIdiomTD \
-    libIdiomTH \
-    libIQQI-jni-Resource \
-    libiqqijni \
-    libiqqipy \
-    libItalian \
-    libjni_kikaime \
-    libmjp \
-    libPortuguese_PT \
-    libRussian \
-    libSpanish_ES \
-
 #Allow resigned images.
 PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
