d46af9e690ae9e1a782cc9479aae314577ed3a17
rumplestilzken <rumplestilzken@gmail.com>
2023-06-30 02:03:35 -0400

    Updated common.mk
    Change-Id: Ia97c6eadaa9099f2acf5509cad6da3b8742a2a05

--- common.mk
+++ common.mk
@@ -2,6 +2,8 @@ RESOURCE_PATH := device/unihertz/
 
 #BOARD_VENDOR_SEPOLICY_DIRS += device/unihertz/sepolicy
 
+TARGET_NO_KERNEL_OVERRIDE := true
+
 #Init Scripts
 PRODUCT_COPY_FILES += \
 	$(RESOURCE_PATH)/init/rumplestilzken_firstboot.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/rumplestilzken_firstboot.rc \
