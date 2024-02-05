97c1c974279f103d684fe83d71ddf64358735269
rumplestilzken <rumplestilzken@gmail.com>
2024-02-05 12:24:42 -0500

    Added kika libraries to system
    Change-Id: If18658362fa03ed53f4e16597f5535100947f1c3

--- lineage_gargoyle.mk
+++ lineage_gargoyle.mk
@@ -36,6 +36,54 @@ PRODUCT_COPY_FILES += \
 	$(RESOURCE_PATH)/keyboard/system_usr_keylayout/Generic.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Generic.kl \
 	$(RESOURCE_PATH)/keyboard/system_usr_keylayout/mtk-kpd.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mtk-kpd.kl \
 
+KIKA := device/unihertz/prebuilt/kika-input/
+
+#lib
+PRODUCT_COPY_FILES += \
+	$(KIKA)/lib/libChangjieD1.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libChangjieD1.so \
+   	$(KIKA)/lib/libChangjieH1.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libChangjieH1.so \
+	$(KIKA)/lib/libChsPinyin.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libChsPinyin.so \
+	$(KIKA)/lib/libChtZhuyin.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libChtZhuyin.so \
+	$(KIKA)/lib/libDutch.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libDutch.so \
+	$(KIKA)/lib/libEnglish_GB.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libEnglish_GB.so \
+	$(KIKA)/lib/libEnglish.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libEnglish.so \
+	$(KIKA)/lib/libFrench_FR.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libFrench_FR.so \
+	$(KIKA)/lib/libGerman.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libGerman.so \
+	$(KIKA)/lib/libIdiomTD.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libIdiomTD.so \
+	$(KIKA)/lib/libIdiomTH.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libIdiomTH.so \
+	$(KIKA)/lib/libIQQI-jni-Resource.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libIQQI-jni-Resource.so \
+	$(KIKA)/lib/libiqqijni.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libiqqijni.so \
+	$(KIKA)/lib/libiqqipy.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libiqqipy.so \
+	$(KIKA)/lib/libItalian.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libItalian.so \
+	$(KIKA)/lib/libjni_kikaime.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libjni_kikaime.so \
+	$(KIKA)/lib/libmjp.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libmjp.so \
+	$(KIKA)/lib/libPortuguese_PT.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libPortuguese_PT.so \
+	$(KIKA)/lib/libRussian.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libRussian.so \
+	$(KIKA)/lib/libSpanish_ES.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libSpanish_ES.so \
+
+#lib64
+PRODUCT_COPY_FILES += \
+	$(KIKA)/lib64/libChangjieD1.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libChangjieD1.so \
+   	$(KIKA)/lib64/libChangjieH1.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libChangjieH1.so \
+	$(KIKA)/lib64/libChsPinyin.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libChsPinyin.so \
+	$(KIKA)/lib64/libChtZhuyin.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libChtZhuyin.so \
+	$(KIKA)/lib64/libDutch.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libDutch.so \
+	$(KIKA)/lib64/libEnglish_GB.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libEnglish_GB.so \
+	$(KIKA)/lib64/libEnglish.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libEnglish.so \
+	$(KIKA)/lib64/libFrench_FR.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libFrench_FR.so \
+	$(KIKA)/lib64/libGerman.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libGerman.so \
+	$(KIKA)/lib64/libIdiomTD.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libIdiomTD.so \
+	$(KIKA)/lib64/libIdiomTH.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libIdiomTH.so \
+	$(KIKA)/lib64/libIQQI-jni-Resource.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libIQQI-jni-Resource.so \
+	$(KIKA)/lib64/libiqqijni.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libiqqijni.so \
+	$(KIKA)/lib64/libiqqipy.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libiqqipy.so \
+	$(KIKA)/lib64/libItalian.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libItalian.so \
+	$(KIKA)/lib64/libjni_kikaime.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libjni_kikaime.so \
+	$(KIKA)/lib64/libmjp.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmjp.so \
+	$(KIKA)/lib64/libPortuguese_PT.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libPortuguese_PT.so \
+	$(KIKA)/lib64/libRussian.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libRussian.so \
+	$(KIKA)/lib64/libSpanish_ES.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libSpanish_ES.so \
+
 #IMS
 PRODUCT_PACKAGES += \
     MTK-IMS-Q \
