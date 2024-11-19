6ca5863f8fe0735f1f81b50f35e11c684bd993b3
rumplestilzken <rumplestilzken@gmail.com>
2024-11-19 10:54:43 -0500

    Added jni needed library

--- uinput-titan/jni/Android.mk
+++ uinput-titan/jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := uinput-titan
 LOCAL_MODULE_HOST_OS := linux
 
-LOCAL_CPPFLAGS := -Wall -Wextra -Wl
+LOCAL_CPPFLAGS := -Wall -Wextra -Wl -std=c++11
 LOCAL_LDLIBS := -llog
 
 LOCAL_SRC_FILES := uinput-titan.c 
@@ -15,7 +15,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libjni_titan
 LOCAL_MODULE_HOST_OS := linux
 
-LOCAL_CPPFLAGS := -Wall -Wextra -Wl
+LOCAL_CPPFLAGS := -Wall -Wextra -Wl -std=c++11
 LOCAL_LDLIBS := -llog
 
 LOCAL_SRC_FILES := uinput-titan.c 
