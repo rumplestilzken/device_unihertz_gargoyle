8f194398d2c279dc1c17a79c20077b2f82e0e820
rumplestilzken <rumplestilzken@gmail.com>
2024-11-19 12:52:50 -0500

    More JNI

--- uinput-titan/jni/Android.mk
+++ uinput-titan/jni/Android.mk
@@ -4,20 +4,18 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := uinput-titan
 LOCAL_MODULE_HOST_OS := linux
 
-LOCAL_CPPFLAGS := -Wall -Wextra -Wl -I${NDK_ROOT}/sysroot/usr/include
+LOCAL_CPPFLAGS := -Wall -Wextra -Wl -I$(TOP)/prebuilts/jdk/jdk17/linux-x86/inlcude/
 LOCAL_LDLIBS := -llog
 
 LOCAL_SRC_FILES := uinput-titan.c 
-
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libjni_titan
 LOCAL_MODULE_HOST_OS := linux
 
-LOCAL_CPPFLAGS := -Wall -Wextra -Wl -I${NDK_ROOT}/sysroot/usr/include
+LOCAL_CPPFLAGS := -Wall -Wextra -Wl -I$(TOP)/prebuilts/jdk/jdk17/linux-x86/inlcude/
 LOCAL_LDLIBS := -llog
 
 LOCAL_SRC_FILES := uinput-titan.c 
-
 include $(BUILD_SHARED_LIBRARY)
