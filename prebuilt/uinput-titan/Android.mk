2f1a63ebea807d34719efabefaeae4baa2627924
rumplestilzken <rumplestilzken@gmail.com>
2024-11-20 12:33:17 -0500

    Release v1.5
    Change-Id: I0b3928295c9608d9ecf25ed44db500f3a6e4a0c3

--- prebuilt/uinput-titan/Android.mk
+++ prebuilt/uinput-titan/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := jni_titanprebuilt
 LOCAL_SRC_FILES := libjni_titan.so
+LOCAL_SHARED_LIBRARIES := liblog libstdc++
 LOCAL_MODULE_TAGS := optional
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -10,5 +11,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := uinput-titan
 LOCAL_MODULE := uinput-titan
 LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SHARED_LIBRARIES := liblog libstdc++
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_PREBUILT)
